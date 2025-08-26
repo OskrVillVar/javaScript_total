b4332a667c422c172909b0dcb254029fc13f0ffd
1551233aaA <gonzalesjosepablo0@gmail.com>
2025-08-26 10:55:56 -0600

    Seccion 4 incompleta

---
+++ seccion_4/4_calcular combustible/misScripts.js
@@ -0,0 +1,11 @@
+function calcularlitros(){
+    var elementoKM = document.getElementById("textokm");
+    var textoKM = elementoKM.value;
+    var cantkm = Number(textoKM);
+
+    var cantlitros = Math.ceil(cantkm / 8.8);
+
+    var resultado = document.getElementById("textoResultado");
+    resultado.textContent = "carga " + cantlitros + " litros de combustible";
+
+}
